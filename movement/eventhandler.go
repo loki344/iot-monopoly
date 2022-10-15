0f8104483cebd9f4af948f749e3d476676c6fbf8
Robert Schlittler <robert.schlittler@inacta.ch>
2022-10-15 10:45:47 +0200

    fix references

--- movement/eventhandler.go
+++ movement/eventhandler.go
@@ -5,17 +5,18 @@ import (
 	"github.com/vmware/transport-go/model"
 	"iot-monopoly/board"
 	"iot-monopoly/eventing"
+	eventingDomain "iot-monopoly/eventing/domain"
 )
 
 func StartEventHandler() {
 
-	channel := eventing.EXTERNAL
+	channel := eventing.MOVEMENT
 
 	sensorEventHandler := eventing.ListenRequestStream(channel)
 
 	sensorEventHandler.Handle(
 		func(msg *model.Message) {
-			sensorEvent := msg.Payload.(eventing.SensorEvent)
+			sensorEvent := msg.Payload.(eventingDomain.SensorEvent)
 			board.MovePlayer(sensorEvent.PlayerId, sensorEvent.FieldIndex)
 		},
 		func(err error) {
