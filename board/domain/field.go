14a42ee6ce99d80070a4295db27fa8326a02b596
Robert Schlittler <robert.schlittler@inacta.ch>
2022-10-15 10:14:58 +0200

    move events to eventingDomain

--- board/domain/field.go
+++ board/domain/field.go
@@ -3,6 +3,7 @@ package boardDomain
 import (
 	"fmt"
 	"iot-monopoly/eventing"
+	"iot-monopoly/eventing/domain"
 )
 
 type Field interface {
@@ -33,7 +34,7 @@ func (eventField EventField) OnPlayerEnter(player *Player) {
 	eventField.Event(player)
 }
 
-func (_ BasicField) OnPlayerEnter(player *Player) {
+func (_ BasicField) OnPlayerEnter(_ *Player) {
 	// do nothing
 }
 
@@ -41,11 +42,11 @@ func (propertyField PropertyField) OnPlayerEnter(player *Player) {
 
 	if propertyField.OwnerId == "" {
 		fmt.Println("property has no owner, is buyable")
-		eventing.FireEvent(eventing.PROPERTY_BUY_QUESTION, eventing.PropertyBuyQuestion{PlayerId: player.Id, PropertyId: propertyField.Id})
+		eventing.FireEvent(eventing.PROPERTY_BUY_QUESTION, eventingDomain.PropertyBuyQuestion{PlayerId: player.Id, PropertyId: propertyField.Id})
 	} else if propertyField.OwnerId == player.Id {
 		fmt.Println("player owns the property..")
 	} else {
 		fmt.Printf("Property belongs to player %s, player %s has to pay %d\n", propertyField.OwnerId, player.Id, propertyField.Price)
-		eventing.FireEvent(eventing.TRANSACTION_REQUESTED, eventing.NewTransactionRequest(propertyField.OwnerId, player.Id, propertyField.Price))
+		eventing.FireEvent(eventing.TRANSACTION_REQUESTED, eventingDomain.NewTransactionRequest(propertyField.OwnerId, player.Id, propertyField.Price))
 	}
 }
