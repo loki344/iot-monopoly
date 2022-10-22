37f18132e36a4011942adaa444595b0d89ef0471
Robert Schlittler <robert.schlittler@inacta.ch>
2022-10-22 21:20:17 +0200

    config files, comment, styling

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -19,7 +19,6 @@ let playerCount = 0
 
 <h1 class="text-3xl font-bold underline">Welcome to IoT-Monopoly</h1>
 <h2>How many are playing?</h2>
-<button class="py-5 px-10 my-5 block bg-gray-200" class:bg-green-200={playerCount == 1} on:click={() => playerCount = 1}>1</button>
 <button class="py-5 px-10 my-5 block bg-gray-200" class:bg-green-200={playerCount == 2} on:click={() => playerCount = 2}>2</button>
 <button class="py-5 px-10 my-5 block bg-gray-200" class:bg-green-200={playerCount == 3} on:click={() => playerCount = 3}>3</button>
 <button class="py-5 px-10 my-5 block bg-gray-200" class:bg-green-200={playerCount == 4} on:click={() => playerCount = 4}>4</button>
