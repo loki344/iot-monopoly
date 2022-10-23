b0d6d6bba93ed6699416f3fb298b3361b06922c2
loki344 <robert.sclittler@gmx.ch>
2022-10-23 17:11:25 +0100

    add rfid communication

---
+++ rfid/raspberry/main.py
@@ -0,0 +1,15 @@
+import serial
+import json
+import requests
+
+ser = serial.Serial('/dev/ttyACM0',9600)
+while True:
+    read_serial=ser.readline().decode("utf-8")
+    obj = json.loads(read_serial)
+    
+    if(obj is not None):
+        requests.patch('http://localhost:3000/players/'+obj['playerId'], json = {'position':obj['fieldId']})
+        print (obj)
+        print (obj['fieldId'])
+        print (obj['playerId'])
+    
